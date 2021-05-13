c0190157a5ed4a3f82fc26499f8356bfa9fbe1be
Logan <logandunne0927@gmail.com>
2021-05-12 19:41:30 -0600

    :)

---
+++ Database-WPF/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Database_WPF.DataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Database_WPF
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public string Name;
+        public string Password;
+
+        // private UserTableAdapter usersTableAdapter;
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Database-WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Database_WPF.DataSetTableAdapters;
+
+namespace Database_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private GameTableAdapter gameTableAdapter = new GameTableAdapter();
+        private DataSet dataSet = new DataSet();
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            gameTableAdapter.Fill(dataSet.Game);
+            DataContext = dataSet.Game.DefaultView;
+        }
+
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            gameTableAdapter.Update(dataSet);
+        }
+    }
+}
